f0191d92e279f10948219b33ceec61c47c0924cb
Omer <omarraza23-12@hotmail.com>
2019-04-10 03:18:22 +0800

    Added apt dependencies in package.json and App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { TouchableOpacity, DrawerActions } from "react-native";
+import { TouchableOpacity, DrawerActions, AsyncStorage } from "react-native";
 import {
   createAppContainer,
   createStackNavigator,
@@ -8,6 +8,15 @@ import {
 import Login from "./screens/Login";
 import HomeScreen from "./screens/Home";
 import { Ionicons } from "@expo/vector-icons";
+import Parse from "parse/react-native"
+
+
+//Parse config done(based on info from PS folder)
+//Import Parse from "parse/react-native" in other js files to "use" it
+
+Parse.initialize("5874f2274cbb3dc45fc8743b4361871278e0c4c1","","6971ed566bb807cd7b7795dd075ea601b4c41e26")
+Parse.serverURL = "http://18.222.127.61:80/parse"
+Parse.setAsyncStorage(AsyncStorage)
 
 const DrawerNavigator = createDrawerNavigator(
   {
